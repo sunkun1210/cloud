ac8ec2bb3eb1831f811c4585fed353c3cb55f64f
sunkun <87983133@qq.com>
2019-12-03 19:44:56 +0800

    优化代理

--- imooc-c5-spring-cloud/proxy/src/main/java/com/imooc/example/proxy/RefrenceAnnotationFactoryBean.java
+++ imooc-c5-spring-cloud/proxy/src/main/java/com/imooc/example/proxy/RefrenceAnnotationFactoryBean.java
@@ -3,17 +3,22 @@ package com.imooc.example.proxy;
 import com.imooc.example.proxy.service.BizService;
 import org.springframework.beans.factory.FactoryBean;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
 /***
  * c、通过FactoryBean实现接口代理
  * @param <T>
  */
-public class RefrenceAnnotationFactoryBean<T> implements FactoryBean<T> {
+public class RefrenceAnnotationFactoryBean<T> implements FactoryBean<T> , InvocationHandler {
 
     public RefrenceAnnotationFactoryBean(){}
-
+    private Class serviceInterface=BizService.class;
     @Override
     public T getObject() throws Exception {
-        return (T) InterfaceProxy.newInstance(BizService.class);
+            return (T) Proxy.newProxyInstance(this.serviceInterface.getClassLoader(), new Class[]{this.serviceInterface}, this);
     }
 
     @Override
@@ -24,5 +29,12 @@ public class RefrenceAnnotationFactoryBean<T> implements FactoryBean<T> {
     @Override
     public boolean isSingleton() {
         return true;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("ObjectProxy execute:" + method.getName());
+        String param = Arrays.toString(args);
+        return "pepsi is param="+param;
     }
 }
